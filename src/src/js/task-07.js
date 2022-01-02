7c141e6ec9eb04f9c6a0673f7c2d728fb00d80a3
Vyacheslav <dikey677@gmail.com>
2022-01-02 20:06:07 +0300

    update-task-07

---
+++ src/src/js/task-07.js
@@ -0,0 +1,13 @@
+let input = document.querySelector("input#font-size-control");
+console.log(input);
+let text = document.querySelector("span#text");
+console.log(text);
+
+input.addEventListener("change", (event) => {
+  let inputChanges = event.currentTarget.value;
+  //   console.log(inputChanges);
+
+  //   console.log(Number(inputChanges));
+
+  text.style.fontSize = `${Number(inputChanges)}px`;
+});
