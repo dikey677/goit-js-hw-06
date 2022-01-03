b11b31299c9751d65c5ad203aff12340d58d4cd4
Vyacheslav <dikey677@gmail.com>
2022-01-03 18:16:09 +0300

    hw-task-09-update

--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector(".widget .change-color");
+// console.log(button);
+const bgColorName = document.querySelector(".widget p .color");
+// console.log(bgColor);
+const body = document.querySelector("body");
+
+button.addEventListener("click", () => {
+  bgColorName.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+});
